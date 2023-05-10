2650121b02671a25822fa62df583b3ad0a9d0261
fayyadarsy <fayyadarsynandi2@gmail.com>
2023-05-11 00:12:01 +0700

    update homepage & plugin parallax

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,10 +6,16 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>ArsyAgro</title>
     <link rel="stylesheet" href="/css/stylehome.css">
+    {{-- Font Awesome --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    {{-- Parralax Plugin --}}
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="/js/parallax.js-1.5.0/parallax.js"></script>
 </head>
 <body>
     {{-- Header --}}
-    <header>
+    <header class="parallax-window" data-parallax="scroll" data-image-src="/images/header-bg.jpg">
         <nav>
             <h1 class="logo"><a href="/">ArsyAgro</a></h1>
             <a href="" class="btn-sign-up">Sign Up</a>
@@ -30,19 +36,19 @@
             <div class="image-gallery">
                 <div class="image-box">
                     <img src="/image/img-1.jpg" alt="image">
-                    <h2 class="bali">B</h2>
+                    <h2 class="arsy">A</h2>
                 </div>
                 <div class="image-box">
                     <img src="/image/img-2.jpg" alt="image">
-                    <h2 class="bali">A</h2>
+                    <h2 class="arsy">R</h2>
                 </div>
                 <div class="image-box">
                     <img src="/image/img-3.jpg" alt="image">
-                    <h2 class="bali">L</h2>
+                    <h2 class="arsy">S</h2>
                 </div>
                 <div class="image-box">
                     <img src="/image/img-4.jpg" alt="image">
-                    <h2 class="bali">I</h2>
+                    <h2 class="arsy">Y</h2>
                 </div>
             </div>
             <div class="about-info">
@@ -51,5 +57,6 @@
         </div>
     </section>
     {{-- Footer --}}
+    <footer>Dibuat Oleh Fayyad  <i class="fa fa-heart"></i></footer>
 </body>
 </html>
